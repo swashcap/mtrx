1465d879c4a727418b8a9a3c4fddc15eaecfc1f4
Cory Reed <swashcap@gmail.com>
2020-03-28 00:45:07 -0700

    chore: add class-properties plugin

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  plugins: [['@babel/plugin-transform-runtime']],
+  plugins: [
+    'babel-plugin-transform-class-properties',
+    '@babel/plugin-transform-runtime',
+  ],
   presets: [
     '@babel/preset-env',
     '@babel/preset-react',
